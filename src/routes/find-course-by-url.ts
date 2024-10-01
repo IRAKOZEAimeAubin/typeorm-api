e67ea1855cdb6273bad0010e65701b2ffe6504a1
Irakoze Aimé Aubin <irakozeaimeaubin@gmail.com>
2024-10-01 15:56:28 +0200

    feat: implement find course by url

--- src/routes/find-course-by-url.ts
+++ src/routes/find-course-by-url.ts
@@ -1,16 +1,46 @@
-import { NextFunction, Request, Response } from "express";
-import { logger } from "../logger";
+import {NextFunction, Request, Response} from 'express'
+import {logger} from '../logger'
+import {AppDataSource} from '../data-source'
+import {Course} from '../models/course'
+import {Lesson} from '../models/lesson'
 
-export async function findCourseByUrl (request:Request,response:Response,next:NextFunction) {
-    try {
-        logger.debug( `Called findCourseByUrl()...` )
-        
-        const courseUrl = request.params.courseUrl
-        
-        if ( !courseUrl ) {
-            throw `Could not request course url from the request`
-        }
-    } catch (error) {
-        logger.error(`Error calling findCourseByUrl()...`)
+export async function findCourseByUrl(
+  request: Request,
+  response: Response,
+  next: NextFunction,
+) {
+  try {
+    logger.debug(`Called findCourseByUrl()...`)
+
+    const courseUrl = request.params.courseUrl
+
+    if (!courseUrl) {
+      throw `Could not request course url from the request`
     }
+
+    const course = await AppDataSource.getRepository(Course).findOneBy({
+      url: courseUrl,
+    })
+
+    if (!course) {
+      const message = `Could not find a course with url ${courseUrl}`
+      logger.error(message)
+      response.status(404).json({message})
+      return
+    }
+
+    const totalLessons = await AppDataSource.getRepository(Lesson)
+      .createQueryBuilder('lessons')
+      .where('lessons.courseId= :courseId', {
+        courseId: course.id,
+      })
+      .getCount()
+
+    response.status(200).json({
+      course,
+      totalLessons,
+    })
+  } catch (error) {
+    logger.error(`Error calling findCourseByUrl()...`)
+  }
 }
